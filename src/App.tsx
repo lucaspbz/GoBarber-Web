03312153b7b0da48e9f66dcd54e82adca1d1eaf3
Lucas Pinheiro <lucas.pbezerra@hotmail.com>
2020-09-01 10:40:47 -0300

    Context integration User authentication

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,15 @@ import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import GlobalStyle from './styles/global';
 
+import { AuthProvider } from './context/AuthContext';
+
 function App() {
   return (
     <>
-      <SignUp />
+      <AuthProvider>
+        <SignIn />
+      </AuthProvider>
+
       <GlobalStyle />
     </>
   );
